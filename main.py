8333a419fa99378d3dcd61122192677137fc33b9
PavanCodes05 <pavancodes05@gmail.com>
2025-10-08 13:41:58 +0530

    complete: dynamic CLI

--- main.py
+++ main.py
@@ -14,17 +14,25 @@ print("Total Members: ", member_size)
 yearwise_count = df['academic_year'].value_counts()
 no_of_seniors = yearwise_count['III']
 
-print("----- Suggestion For Team Size -----")
+suggested_sizes = set()
+
+print("----- Suggested Number Of Teams -----")
 for size in range(1, member_size):
     if member_size % size == 0:
         senior_split = (member_size / size)
         if senior_split <= no_of_seniors:
+            suggested_sizes.add(size)
             print(size, end=' ')
 print("\n")
 
 # Random Pick Logic
 # Get Input From User
-split_size = 4 # Indicates total number of team
+while True:
+    split_size = int(input("Enter Number Of Teams Required: "))
+    if split_size in suggested_sizes:
+        break
+    print("Please Pick From The Suggested Size!")
+
 team_size = (member_size // split_size)
 
 team_split_info = defaultdict(list)
