f5522073db2fc7113e5829bc74ad5ff844c778b2
PavanCodes05 <pavancodes05@gmail.com>
2025-10-08 11:20:58 +0530

    feat: size suggestion logic

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
+# Import Statements
 import pandas
 
-csv_file = pandas.read_csv('mock.csv')
-print(csv_file)
+# Read The CSV file
+df = pandas.read_csv('mock.csv', usecols=['Full Name', 'Academic Year'])
+
+# Split Suggestion
+member_size = len(df)
+print("Total Members: ", member_size)
+
+yearwise_count = df['Academic Year'].value_counts()
+no_of_seniors = yearwise_count['III']
+
+print("----- Suggestion For Team Size -----")
+for size in range(1, member_size):
+    if member_size % size == 0:
+        senior_split = (member_size / size)
+        if senior_split <= no_of_seniors:
+            print(size, end='\n')
